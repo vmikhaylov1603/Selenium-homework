986ced8973baa4231502addb07ad478174f7dff3
vyacheslav <1>
2016-12-17 00:36:08 +0700

    тест логина

---
+++ 3/login.js
@@ -0,0 +1,29 @@
+var webdriver = require('selenium-webdriver'),
+    chrome = require('selenium-webdriver/chrome'),
+    By = webdriver.By,
+    until = webdriver.until,
+    test = require('selenium-webdriver/testing');
+
+test.describe('Простой тест для логина и пароля', function() {
+    var driver;
+
+    test.before(function() {
+        driver = new webdriver.Builder()
+            .forBrowser('chrome')
+            .build();
+        driver.getCapabilities().then(function(caps) {
+            console.log(caps);
+        });
+    });
+
+    test.it('Должен работать вход под админом', function() {
+        driver.get('http://localhost/litecart/admin/');
+        driver.findElement(By.name('username')).sendKeys('admin');
+        driver.findElement(By.name('password')).sendKeys('admin');
+        driver.findElement(By.name('login')).click();
+    });
+
+    test.after(function() {
+        driver.quit();
+    });
+});
